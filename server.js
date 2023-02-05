895f12ce4e9ce74b01614e61bb23dce8bc35aaeb
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-02-05 22:12:11 +0530

    hi

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use("/student",studentRouter);
 app.use(express.static(path.join(__dirname,"./frontend/build")));
 app.get("*",function(_,res){
     res.sendFile(
-        path.join(__dirname,"frontend/build/index.html"),
+        path.join(__dirname,"./frontend/build/index.html"),
         function(err){
             res.status(500).send(err);
         }
